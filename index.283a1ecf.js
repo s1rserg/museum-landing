7708b9eea6a828e1fe135a1375089ff915137bf0
sirserg <sirserg05@gmail.com>
2025-06-16 12:03:43 +0300

    make build

---
+++ index.283a1ecf.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{0:{slidesPerView:1.06755,spaceBetween:16},768:{slidesPerView:2.0805,spaceBetween:20}}});
+//# sourceMappingURL=index.283a1ecf.js.map
